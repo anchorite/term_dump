6b6db100430876c572215111d0cb996439f478bc
Alexander Dimitrov <3321085+anchorite@users.noreply.github.com>
2020-08-05 01:30:05 +0300

    Adds dump of numeric values.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ fn dump_boolean(term: &UnibiTerm) {
     }
 }
 
+fn dump_numeric(term: &UnibiTerm) {
+    for (name, value) in term.iter_numeric() {
+        println!("{}: {}", name, value);
+    }
+}
+
 fn main() {
     let matches = App::new("TermDump")
         .about("Dump information stored in terminfo database")
@@ -22,9 +28,17 @@ fn main() {
                 .long("boolean")
                 .help("Dump boolean capabilities."),
         )
+        .arg(
+            Arg::with_name("numeric")
+                .short("n")
+                .long("numeric")
+                .help("Dump numeric capabilities."),
+        )
         .get_matches();
     let term = UnibiTerm::from_env();
     if matches.is_present("boolean") {
         dump_boolean(&term);
+    } else if matches.is_present("numeric") {
+        dump_numeric(&term);
     }
 }
