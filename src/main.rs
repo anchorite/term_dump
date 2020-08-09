d604417dda06548edf364aa11d6b6ddc3c610c55
Alexander Dimitrov <3321085+anchorite@users.noreply.github.com>
2020-08-09 20:29:08 +0300

    Switches to using collections of capabilities.

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,32 @@ use clap::{App, Arg};
 use unibilium::*;
 
 fn dump_boolean(term: &UnibiTerm) {
-    for (name, value) in term.iter_bool() {
+    for (name, value) in term.booleans().iter() {
         println!("{}: {}", name, value);
     }
 }
 
+fn dump_xboolean(term: &UnibiTerm) {
+    for (name, value) in term.ext_booleans().iter() {
+        match name {
+            Some(name) => println!("{}: {}", name, value),
+            None => println!("NULL: {}", value),
+        }
+    }
+}
+
 fn dump_numeric(term: &UnibiTerm) {
-    for (name, value) in term.iter_numeric() {
+    for (name, value) in term.numerics().iter() {
         println!("{}: {}", name, value);
+    }
+}
+
+fn dump_string(term: &UnibiTerm) {
+    for (name, value) in term.strings().iter() {
+        match value {
+            None => println!("{}: NULL", name),
+            Some(value) => println!("{}: {}", name, value),
+        }
     }
 }
 
@@ -30,10 +48,21 @@ fn main() {
                 .help("Dump boolean capabilities."),
         )
         .arg(
+            Arg::with_name("ext-boolean")
+                .long("xboolean")
+                .help("Dump extended boolean capabilities."),
+        )
+        .arg(
             Arg::with_name("numeric")
                 .short("n")
                 .long("numeric")
                 .help("Dump numeric capabilities."),
+        )
+        .arg(
+            Arg::with_name("string")
+                .short("s")
+                .long("string")
+                .help("Dump string capabilities."),
         )
         .get_matches();
     let term = match matches.value_of("terminal") {
@@ -44,5 +73,9 @@ fn main() {
         dump_boolean(&term);
     } else if matches.is_present("numeric") {
         dump_numeric(&term);
+    } else if matches.is_present("string") {
+        dump_string(&term);
+    } else if matches.is_present("ext-boolean") {
+        dump_xboolean(&term);
     }
 }
